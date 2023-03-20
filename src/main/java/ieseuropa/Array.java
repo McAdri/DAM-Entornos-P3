ddac22513a444a02d3d03621f953b54519147c28
PatriDominguez <122823076+PatriDominguez@users.noreply.github.com>
2023-03-20 19:42:51 +0100

    NUEVO

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -7,6 +7,8 @@ public class Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
-	}
+		System.out.println("Adios");
+		System.out.println("Hola");
+	}    
 
 }
