cbdcc14a359035c85129c50d2ab3bba66c4cb52e
OscarDH <chusmita.oscar@gmail.com>
2023-03-27 19:53:54 +0200

    max array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,9 +2,18 @@ package main.java.ieseuropa;
 
 public class Array {
 
+	private static int maxArray(int[] array) {
+		int max = 0;
+		for (int num : array) {
+			if (num > max)
+				max = num;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(maxArray(array));
 	}
 
 }
