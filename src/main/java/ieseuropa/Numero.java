2baf485c004cfd6a8909375fa13677e0b27eb87d
KIKE <KIKE@DIEZ>
2023-03-30 16:19:40 +0200

    -Redondear un numero al alza Este metodo recibirá un float y deberá devolver un int. Redondearemos siempre al alza, tenga los decimales que tenga(siempre que tenga decimales)
    -Eliminar los decimales de un numero
    Este metodo recibirá un float y devolverá un int. Simplemente devolverá
    la parte entera, ignorando los decimales.
    
    -Dados tres coeficientes ejecutar la ecuacion de segundo grado.
    Este metodo recibirá tres enteros y devolverá un String, el cual
    indicará si tiene una, dos o ninguna solucion (e indicando las
    soluciones)

--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -43,12 +43,37 @@ public class Numero {
 			return false;
 		}
 	}
+	
 
+	public static double redondearAlAlza(float numero) {
+		return Math.ceil(numero);
+	}
+	
+	public static int eliminarDecimales(float numero) {
+		return (int) numero;
+	}
+
+	public static void ecuacionSegundoGrado(int a, int b, int c) {
+		float discriminante = b * b - 4 * a * c;
+		if (discriminante < 0) {
+			System.out.println("La ecuación no tiene soluciones reales.");
+		} else if (discriminante == 0) {
+			double x = -b / (2 * a);
+			System.out.println("La ecuación tiene una solución real: " + x);
+		} else {
+			double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+			double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+			System.out.println("La ecuación tiene dos soluciones reales: " + x1 + " y " + x2);
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(seriePell());
 		System.out.println(numArmstrong(150));
 		System.out.println(numSuerte(87));
-
+		System.out.println(redondearAlAlza((float) 23.45));
+		System.out.println(eliminarDecimales((float) 23.45));
+		ecuacionSegundoGrado(2,-7,3);
 	}
 
 }
