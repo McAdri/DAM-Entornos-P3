497f8d316485f20df3e24caddd2869ebad15886f
AitorSV <aitorsuarez.vic@gmail.com>
2023-03-30 16:48:26 +0200

    metodos

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -20,7 +20,33 @@ public class Array {
 		}
 		return minimo;
 	}
-
+	public static int[] arrayMenorAMayor(int[] datos) {
+		for (int i = 0; i < datos.length - 1; i++) {
+			for (int j = 0; j < datos.length - i - 1; j++) {
+				if (datos[j] > datos[j + 1]) {
+					int temp = datos[j];
+					datos[j] = datos[j + 1];
+					datos[j + 1] = temp;
+				}
+			}
+		}
+		return datos;
+	}
+	public static float mediana (int[] datos) {
+		int [] ordenada= arrayMenorAMayor(datos);
+		float mediana;
+		if(ordenada.length%2!=0) {
+			mediana=ordenada[ordenada.length/2];
+		}
+		else {
+			float m1=ordenada[ordenada.length/2];
+			System.out.println(m1);
+			float m2=ordenada[ordenada.length/2-1];
+			System.out.println(m2);
+			mediana=(m1+m2)/2;
+		}
+		return mediana;
+	}
 	public static void main(String[] args) {
 		int[] array = { 1, 2, 3, 4, 5 };
 		System.out.println(maxArray(array));
